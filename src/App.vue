999ffcb66ec65cdafeef93cabf366ef67b1c3dcd
suhyeoney <sooheyong102@gmail.com>
2022-06-20 11:10:34 +0900

    Vue-d3-network library applied

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SampleTopology msg="여기부터는 Topology 영역입니다." />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SampleTopology from './components/network/SampleTopology.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SampleTopology
   }
 }
 </script>
---
+++ src/components/network/SampleTopology.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <d3-network :net-nodes="nodes" :net-links="links" :options="options" />
+  </div>
+</template>
+
+<script>
+import D3Network from 'vue-d3-network'
+
+export default {
+  name: 'SampleTopology',
+  components: {
+    D3Network
+  },
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+        nodes: [
+            {id: 1, name: "Node1"},
+            {id: 2, name: "Node2"}
+        ],
+        links: [
+            {sid: 1, tid: 2, _color: "red"}
+        ],
+        options: {
+            force: 3000,
+            nodeSize:15,
+            nodeLabels: true,
+            linkWidth: 5
+        }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
