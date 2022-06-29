561e451b321ae0d7f9066db4b2fd8f3ec1c922cd
suhyeong.ahn <suhyeong.ahn@bespinglobal.com>
2022-06-29 18:00:28 +0900

    Implement EditToggleButton Component (Doing)

---
+++ src/components/common/EditToggleButton.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    name: 'EditToggleButton',
+    extends: EditToggleButton, // extends : "하나만" 상속 가능 하지만 style, template 가져올수 있다. ( 기능 + ui or ui 분리 )
+    props: {
+    },
+    methods: {
+        toggleButton() {
+            
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
