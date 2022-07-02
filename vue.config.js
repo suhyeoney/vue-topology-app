76245a085878950830883e35775f5e2de7d68742
suhyeong.ahn <suhyeong.ahn@bespinglobal.com>
2022-07-02 21:36:14 +0900

    Set ESLintOnSave property to false

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false, // ESLint와 컴포넌트명 규칙 충돌을 방지하기 위해 false로 설정
 })
